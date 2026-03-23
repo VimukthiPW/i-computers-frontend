feed4b16f1d1b67a06f6ee7aa054ca9439225e95
VimukthiPW <priyadarshanvimukthi7@gmail.com>
2026-03-23 19:53:22 +0530

    Day 17✏️

--- src/pages/admin.jsx
+++ src/pages/admin.jsx
@@ -1,25 +1,25 @@
 import { Link, Route, Routes } from "react-router-dom";
+import { FaRegListAlt } from "react-icons/fa";
+import { MdOutlineInventory2 } from "react-icons/md";
+import { LuUsers } from "react-icons/lu";
+import AdminProductsPage from "./admin/adminProductsPage";
+import AdminAddProductPage from "./admin/adminAddProductPage";
 
 export default function AdminPage(){
     return(
-        <div className="w-full h-full border-4 border-blue-900 flex">
-            <div className="w-[300px] h-full bg-red-900 flex flex-col">
-                <h1 className="font-bold">Using a tags</h1>
-                <a href="/admin/">Admin</a><br/>
-                <a href="/admin/products">Products</a><br/>
-                <a href="/admin/users">Users</a><br/>
-
-                <h1 className="font-bold">Using Link tags</h1>
-                <Link to="/admin/">Admin</Link>
-                <Link to="/admin/products">Product</Link>
-                <Link to="/admin/users">Users</Link>
-
+        <div className="w-full h-full border-4 border-blue-900 bg-accent flex">
+            <div className="w-[300px] h-full flex flex-col bg-accent text-white">
+                <h1 className="font-bold p-5 border-b-4 border-white">Admin panel</h1>
+                <Link className="flex w-full p-[10px] gap-3 items-center hover:bg-white hover:text-accent" to="/admin/"> <FaRegListAlt/> Order</Link>
+                <Link className="flex w-full p-[10px] gap-3 items-center hover:bg-white hover:text-accent" to="/admin/products"><MdOutlineInventory2 />Product</Link>
+                <Link className="flex w-full p-[10px] gap-3 items-center hover:bg-white hover:text-accent" to="/admin/users"><LuUsers />Users</Link>
             </div>
-            <div className="w-[calc(100%-300px)] h-full bg-amber-400">
+            <div className="w-[calc(100%-300px)] h-full border-8 border-accent rounded-[20px] bg-primary p-4">
                 <Routes>
                     <Route path="/" element={<h1>Order Page</h1>} />
-                    <Route path="/products" element={<h1>Product Page</h1>} />
+                    <Route path="/products" element={<AdminProductsPage/>} />
                     <Route path="/users" element={<h1>Users Page</h1>} />
+                    <Route path="/add-product" element={<AdminAddProductPage/>}/>
                 </Routes>
 
             </div>
